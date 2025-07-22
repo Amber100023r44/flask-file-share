6681609b01d843a0f06629f9cea96060c12dce3f
Amber100023r44 <itztherc@gmail.com>
2025-07-23 04:21:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,4 +33,6 @@ def download(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
+
