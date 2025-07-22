982e8d1021dc0835d57d6af3f2e36e3bf302d5ed
Amber100023r44 <itztherc@gmail.com>
2025-07-23 04:25:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, redirect, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, redirect, url_for
 import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @app.route('/')
 def index():
@@ -14,23 +16,25 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     if 'file' not in request.files:
-        return redirect('/')
+        return 'No file part'
     file = request.files['file']
     if file.filename == '':
-        return redirect('/')
-    filename = secure_filename(file.filename)
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(filepath)
-    return redirect('/files')
+        return 'No selected file'
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return redirect(url_for('files'))
 
 @app.route('/files')
 def files():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('files.html', files=files)
+    file_list = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('files.html', files=file_list)
 
-@app.route('/download/<filename>')
-def download(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
